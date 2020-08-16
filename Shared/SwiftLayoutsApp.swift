c90cfbd66e5b9a1af8d0ac59451b67b573763883
Shiva Narrthine <shivanarrthine@gmail.com>
2020-08-17 06:50:40 +0800

    Initial Commit

---
+++ Shared/SwiftLayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLayoutsApp.swift
+//  Shared
+//
+//  Created by Shiva Narrthine on 17/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
